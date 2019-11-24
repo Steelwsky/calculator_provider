de02527a0e242b7aaf72e8600a8eebaca42cc9b7
steelwsky <zero.rise@yandex.ru>
2019-11-25 00:49:04 +1000

    onEqualPressed() fails. currentResult is string of numbers and operators. To deal with it, i have to set separate them. TDD! don't forget! failed tests (expected behavior) THEN functions THEN successfully finished tests.

--- lib/calc_controller.dart
+++ lib/calc_controller.dart
@@ -22,4 +22,24 @@ class CalcController {
       state.value = newState;
     }
   }
+
+  void onOperationPressed(String operator) {
+    final currentResult = state.value.currentResult;
+    if (currentResult.contains('+')) {
+      return;
+    } else {
+      final newState = CalcState(currentResult: currentResult + operator);
+      state.value = newState;
+    }
+  }
+
+  void onEqualPressed() {
+    final currentResult = state.value.currentResult;
+    if (currentResult.contains('+')) {
+        List splitResult = currentResult.split('+');
+        print('splitResult: ${splitResult[0]}, ${splitResult[1]}, ${splitResult[2]}, ${splitResult[3]}');
+        final newState  = CalcState(currentResult: (double.parse(splitResult[0]) + double.parse(splitResult[2])).toString());
+        state.value = newState;
+    }
+  }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -32,9 +32,90 @@ void main() {
     thenResultShouldBe('11');
   });
 
-  testWidgets(
-      'should show + after user press +', (WidgetTester tester) async {},
-      skip: true);
+  testWidgets('should show + after user press +', (WidgetTester tester) async {
+    await givenAppIsPumped(tester);
+    thenResultShouldBe('0');
+    await whenUserPressesButton(button1, tester);
+    thenResultShouldBe('1');
+    await whenUserPressesButton(plusButton, tester);
+    thenResultShouldBe('1+');
+  });
+
+  testWidgets('should show only one + after user press + again', (WidgetTester tester) async {
+    await givenAppIsPumped(tester);
+    thenResultShouldBe('0');
+    await whenUserPressesButton(button1, tester);
+    thenResultShouldBe('1');
+    await whenUserPressesButton(plusButton, tester);
+    thenResultShouldBe('1+');
+    await whenUserPressesButton(plusButton, tester);
+    thenResultShouldBe('1+');
+  });
+
+  testWidgets('should show 1+1', (WidgetTester tester) async {
+    await givenAppIsPumped(tester);
+    thenResultShouldBe('0');
+    await whenUserPressesButton(button1, tester);
+    thenResultShouldBe('1');
+    await whenUserPressesButton(plusButton, tester);
+    thenResultShouldBe('1+');
+    await whenUserPressesButton(button1, tester);
+    thenResultShouldBe('1+1');
+  });
+
+  testWidgets('should show 0+ after user tapped @+button@ after app is pumped', (WidgetTester tester) async {
+    await givenAppIsPumped(tester);
+    thenResultShouldBe('0');
+    await whenUserPressesButton(plusButton, tester);
+    thenResultShouldBe('0+');
+  });
+
+  testWidgets('should show 0+ after user tapped @+button@ after app is pumped', (WidgetTester tester) async {
+    await givenAppIsPumped(tester);
+    thenResultShouldBe('0');
+    await whenUserPressesButton(plusButton, tester);
+    thenResultShouldBe('0+');
+  });
+
+  testWidgets('should show 2 after user tapped 1, +, 1 and = after all', (WidgetTester tester) async {
+    await givenAppIsPumped(tester);
+    thenResultShouldBe('0');
+    await whenUserPressesButton(button1, tester);
+    thenResultShouldBe('1');
+    await whenUserPressesButton(plusButton, tester);
+    thenResultShouldBe('1+');
+    await whenUserPressesButton(plusButton, tester);
+    thenResultShouldBe('1+1');
+    await whenUserPressesButton(equalButton, tester);
+    thenResultShouldBe('2');
+  });
+
+  testWidgets('should show 0 after user tapped + and = after app is pumped', (WidgetTester tester) async {
+    await givenAppIsPumped(tester);
+    thenResultShouldBe('0');
+    await whenUserPressesButton(plusButton, tester);
+    thenResultShouldBe('0+');
+    await whenUserPressesButton(equalButton, tester);
+    thenResultShouldBe('0');
+  });
+
+  testWidgets('should show 1 after user tapped +, 1 and = after app is pumped', (WidgetTester tester) async {
+    await givenAppIsPumped(tester);
+    thenResultShouldBe('0');
+    await whenUserPressesButton(plusButton, tester);
+    thenResultShouldBe('0+');
+    await whenUserPressesButton(button1, tester);
+    thenResultShouldBe('0+1');
+    await whenUserPressesButton(equalButton, tester);
+    thenResultShouldBe('1');
+  });
+
+}
+
+
+Future<void> givenAppIsPumped(WidgetTester tester) async {
+  await tester.pumpWidget(MyApp());
+  await tester.pump();
 }
 
 Future<void> whenUserPressesButton(Finder toPress, WidgetTester tester) async {
@@ -42,18 +123,17 @@ Future<void> whenUserPressesButton(Finder toPress, WidgetTester tester) async {
   await tester.pump();
 }
 
-Finder get textViewResultFinder => find.byKey(ValueKey('textViewResult'));
-
-Finder get button1 => find.byKey(ValueKey('button1'));
-
-String get currentResultText =>
-    (textViewResultFinder.evaluate().single.widget as Text).data;
-
 void thenResultShouldBe(String text) {
   expect(currentResultText, text);
 }
 
-Future<void> givenAppIsPumped(WidgetTester tester) async {
-  await tester.pumpWidget(MyApp());
-  await tester.pump();
-}
+Finder get textViewResultFinder => find.byKey(ValueKey('textViewResult'));
+
+Finder get button1 => find.byKey(ValueKey('button1'));
+
+Finder get plusButton => find.byKey(ValueKey('plusButton'));
+
+Finder get equalButton => find.byKey(ValueKey('equalButton'));
+
+String get currentResultText =>
+    (textViewResultFinder.evaluate().single.widget as Text).data;
